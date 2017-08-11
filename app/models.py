847f9689ad2e06df8511d2a631debb6510b1691c
hewei <dd@qq.com>
2017-08-11 17:27:54 +0800

    增加列表页

--- app/models.py
+++ app/models.py
@@ -8,6 +8,14 @@ class Categories(Document):
     artnum = IntField(default=0, required=True)
     date = DateTimeField(default=datetime.now(), required=True)
 
+class  Keywords(Document):
+    md5_string = StringField(max_length=32, required=True)
+    title = db.StringField()  #标题
+
+class WXUrls(Document):
+    md5_str = StringField(max_length=32, required=True)
+    wx_url = db.StringField()  #标题
+
 class  Contents(EmbeddedDocument):
     name = StringField()
     date = DateTimeField(default=datetime.now())
@@ -24,5 +32,7 @@ class Detail(db.Document):
     description = db.StringField()  #描述
     #categories = ReferenceField(Categories)  #分类
     contents = EmbeddedDocumentField('Contents')   #内容
+
+
 
 
